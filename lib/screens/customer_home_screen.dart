ada515e91d5ad5254e839fc030a68ff89c9f5beb
noufel-boulkroune <noufelboulkroune@gmail.com>
2022-12-15 13:59:58 +0100

    prepare the bottom nav bar

--- lib/screens/customer_home_screen.dart
+++ lib/screens/customer_home_screen.dart
@@ -9,13 +9,23 @@ class CustomerHomeScreen extends StatefulWidget {
 
 class _CustomerHomeScreenState extends State<CustomerHomeScreen> {
   int _selectedIndex = 0;
+  final List<Widget> _tabs = const [
+    Center(child: Text("Home Screen")),
+    Center(child: Text("Category Screen")),
+    Center(child: Text("Stores Screen")),
+    Center(child: Text("Cart Screen")),
+    Center(child: Text("Profile Screen")),
+  ];
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.white,
+      body: _tabs[_selectedIndex],
       bottomNavigationBar: BottomNavigationBar(
+        type: BottomNavigationBarType.fixed,
         selectedItemColor: Colors.blue,
-        unselectedItemColor: Colors.black,
+        unselectedItemColor: Colors.grey,
+        elevation: 0,
+        selectedLabelStyle: TextStyle(fontWeight: FontWeight.w600),
         currentIndex: _selectedIndex,
         items: const [
           BottomNavigationBarItem(
@@ -28,7 +38,7 @@ class _CustomerHomeScreenState extends State<CustomerHomeScreen> {
           ),
           BottomNavigationBarItem(
             icon: Icon(Icons.shop),
-            label: "Store",
+            label: "Stores",
           ),
           BottomNavigationBarItem(
             icon: Icon(Icons.shopping_cart),
