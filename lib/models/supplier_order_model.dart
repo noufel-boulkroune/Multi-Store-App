d79b722173eee3e2df96a1a922a675f1a571c4cb
noufel-boulkroune <noufelboulkroune@gmail.com>
2023-01-04 11:40:20 +0100

    Stream supplier orders data and add supllier order screen

---
+++ lib/models/supplier_order_model.dart
@@ -0,0 +1,152 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+import 'package:flutter_datetime_picker/flutter_datetime_picker.dart';
+
+class SupplierOrderModel extends StatelessWidget {
+  final dynamic order;
+  SupplierOrderModel({super.key, required this.order});
+
+  @override
+  Widget build(BuildContext context) {
+    return ExpansionTile(
+      title: Container(
+        constraints:
+            const BoxConstraints(maxHeight: 80, maxWidth: double.infinity),
+        child: Row(
+          children: [
+            Container(
+              constraints: const BoxConstraints(maxHeight: 80, maxWidth: 80),
+              child: Image.network(order["orderImage"]),
+            ),
+            Flexible(
+                child: Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Text(
+                    order["productName"],
+                    overflow: TextOverflow.ellipsis,
+                    maxLines: 2,
+                    style: TextStyle(
+                        fontSize: 12,
+                        color: Colors.grey.shade600,
+                        fontWeight: FontWeight.w600),
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Text("\$ ${order["orderPrice"].toStringAsFixed(2)}"),
+                      Text("x ${order["orderQuantity"].toString()}")
+                    ],
+                  )
+                ],
+              ),
+            ))
+          ],
+        ),
+      ),
+      subtitle: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [const Text("See more .."), Text(order["deliveryStatus"])],
+      ),
+      children: [
+        Container(
+          // height: 230,
+          width: double.infinity,
+          decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(15), color: Colors.white),
+          child: Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  "Name: ${order["customerName"]}",
+                  style: const TextStyle(fontSize: 15),
+                ),
+                Text(
+                  "Phone: ${order["customerPhone"]}",
+                  style: const TextStyle(fontSize: 15),
+                ),
+                Text(
+                  "Email Address: ${order["customerEmail"]}",
+                  style: const TextStyle(fontSize: 15),
+                ),
+                Text(
+                  "Address: ${order["customerAddress"]}",
+                  style: const TextStyle(fontSize: 15),
+                ),
+                Row(
+                  children: [
+                    const Text(
+                      "Payment Status: ",
+                      style: TextStyle(fontSize: 15),
+                    ),
+                    Text(
+                      "${order["paymentStatus"]}",
+                      style:
+                          const TextStyle(fontSize: 15, color: Colors.purple),
+                    ),
+                  ],
+                ),
+                Row(
+                  children: [
+                    const Text(
+                      "Delivry Status: ",
+                      style: TextStyle(fontSize: 15),
+                    ),
+                    Text(
+                      "${order["deliveryStatus"]}",
+                      style: const TextStyle(fontSize: 15, color: Colors.green),
+                    ),
+                  ],
+                ),
+                Row(
+                  children: [
+                    const Text(
+                      "Order Date: ",
+                      style: TextStyle(fontSize: 15),
+                    ),
+                    Text(
+                      "${DateFormat.yMMMd().format(order["orderDate"].toDate())}",
+                      style: const TextStyle(fontSize: 15, color: Colors.green),
+                    ),
+                  ],
+                ),
+                order["deliveryStatus"] == "delivered"
+                    ? const Text(
+                        "This order has been already delivered",
+                        style: TextStyle(color: Colors.lightBlue),
+                      )
+                    : Row(
+                        children: [
+                          const Text(
+                            "Change Delivery Status To: ",
+                            style: TextStyle(fontSize: 15),
+                          ),
+                          order["deliveryStatus"] == "preparing"
+                              ? TextButton(
+                                  onPressed: () {
+                                    DatePicker.showDatePicker(
+                                      context,
+                                      minTime: DateTime.now(),
+                                      maxTime: DateTime.now()
+                                          .add(const Duration(days: 365)),
+                                      onConfirm: (date) async {},
+                                    );
+                                  },
+                                  child: Text("Shipping ?"))
+                              : TextButton(
+                                  onPressed: () {}, child: Text("Delivered ?"))
+                        ],
+                      ),
+              ],
+            ),
+          ),
+        )
+      ],
+    );
+  }
+}
--- lib/screens/dashboard_components/supplier_orders_screen.dart
+++ lib/screens/dashboard_components/supplier_orders_screen.dart
@@ -1,17 +1,54 @@
 import 'package:flutter/material.dart';
 import 'package:multi_store_app/widgets/appbar_widget.dart';
 
+import 'delivered_screen.dart';
+import 'preparing_screen.dart';
+import 'shipping_screen.dart';
+
 class SupplierOrdersScreen extends StatelessWidget {
   const SupplierOrdersScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        elevation: 0,
-        title: const AppBarTitle(title: "Supplier Orders"),
-        leading: AppBarBackButton(
-          color: Colors.black,
+    return DefaultTabController(
+      length: 3,
+      child: Scaffold(
+          appBar: AppBar(
+            backgroundColor: Colors.white,
+            elevation: 0,
+            title: const AppBarTitle(title: "Orders"),
+            leading: AppBarBackButton(
+              color: Colors.black,
+            ),
+            bottom: const TabBar(
+                indicatorWeight: 4,
+                indicatorColor: Colors.lightBlueAccent,
+                tabs: [
+                  RepeatedTab(label: "Preparing"),
+                  RepeatedTab(label: "Shipping"),
+                  RepeatedTab(label: "Delivered"),
+                ]),
+          ),
+          body: TabBarView(children: [
+            PreparingScreen(),
+            ShippingScreen(),
+            DeliveredScreen()
+          ])),
+    );
+  }
+}
+
+class RepeatedTab extends StatelessWidget {
+  final String label;
+  const RepeatedTab({super.key, required this.label});
+
+  @override
+  Widget build(BuildContext context) {
+    return Tab(
+      child: Center(
+        child: Text(
+          label,
+          style: const TextStyle(color: Colors.grey),
         ),
       ),
     );
